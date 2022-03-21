3a1caaeb8187acbd97483689fd86c776c2e2c630
Chris Coey <coey.chris@gmail.com>
2022-03-21 11:43:51 -0400

    adjust benchmarks

--- examples/ballpacking/instances.jl
+++ examples/ballpacking/instances.jl
@@ -14,16 +14,16 @@ insts["test"] = [
     # ((1, 2, LogIBNonneg(), 11), nosubp_options),
 ]
 
-function ballpacking_insts(pwl::PWLSOS2)
+function ballpacking_insts(pwl::PWLSOS2, max_p::Int)
     ts = [((4, 3, pwl, 7), nosubp_options)] # compile
-    for p in 3:13
+    for p in 3:max_p
         push!(ts, ((4, 3, pwl, 2^p - 1), nosubp_options))
     end
     return ts
 end
 
-insts["sos2"] = ballpacking_insts(SOS2())
-insts["logib"] = ballpacking_insts(LogIBBounded())
-insts["cc"] = ballpacking_insts(CCBounded())
+insts["sos2"] = ballpacking_insts(SOS2(), 10)
+insts["logib"] = ballpacking_insts(LogIBBounded(), 13)
+insts["cc"] = ballpacking_insts(CCBounded(), 7)
 
 return (BallPacking, insts)
--- examples/inversecovariance/instances.jl
+++ examples/inversecovariance/instances.jl
@@ -8,9 +8,9 @@ insts["test"] = [
     ((6, MatNegRtdetEFExp()),),
 ]
 
-inversecovariance_insts(f::MatSpecExt, max_d::Int) = [((d, f),) for d in vcat(5, 5:2:max_d)]
+inversecovariance_insts(f::MatSpecExt, max_d::Int) = [((d, f),) for d in vcat(5, 5:max_d)]
 
-insts["nat"] = inversecovariance_insts(MatNegLog(), 25)
-insts["ext"] = inversecovariance_insts(MatNegLogDirect(), 25)
+insts["nat"] = inversecovariance_insts(MatNegLog(), 16)
+insts["ext"] = inversecovariance_insts(MatNegLogDirect(), 16)
 
 return (InverseCovariance, insts)
